512c7478d753de4f621bf610bfc6721e0437d413
Syed Wajeeh ul Hassan <wajeeh2019@namal.edu.pk>
2021-10-02 21:26:51 +0500

    Sub Functionality

---
+++ cal05.py
@@ -0,0 +1,13 @@
+calculator in Python(v0.5)
+def sqRoot(num1):
+	return num1**0.5
+def div(num1,num2):
+	return num1/num2
+def mul(num1,num2):
+	return num1*num2
+def add(num1,num2):
+	return num1+num2
+def sub(num1,num2):
+	return num1-num2
+# main
+print(sub(7,2),add(4,5),mul(5,2),div(4,2),sqRoot(9))
