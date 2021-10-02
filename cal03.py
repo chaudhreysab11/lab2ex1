d03d9044cb0bb9cce0afdb2c3962460bdda30afd
Hurmat Ilyas <hurmat2019@namal.edu.pk>
2021-10-02 20:40:17 +0500

    Mul Functionality

---
+++ cal03.py
@@ -0,0 +1,9 @@
+calculator in Python(v0.1)
+def sqRoot(num1):
+	return num1**0.5
+def div(num1,num2):
+	return num1/num2
+def mul(num1,num2):
+	return num1*num2
+# main
+print(mul(5,2),div(4,2),sqRoot(9))
