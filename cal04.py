7086ec4db12a5458d0c1ac4d81a3506321c426e0
Muhammad Bilal <mbilal2019@namal.edu.pk>
2021-10-02 21:24:10 +0500

    Add Functionality

---
+++ cal04.py
@@ -0,0 +1,11 @@
+calculator in Python(v0.3)
+def sqRoot(num1):
+	return num1**0.5
+def div(num1,num2):
+	return num1/num2
+def mul(num1,num2):
+	return num1*num2
+def add(num1,num2):
+	return num1+num2
+# main
+print(add(4,5),mul(5,2),div(4,2),sqRoot(9))
